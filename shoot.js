82f02cf227b29a900edb0818ea85cb41baa068ab
ksps7722 <88386473+ksps7722@users.noreply.github.com>
2023-06-04 16:55:21 +0530

    Add files via upload

---
+++ shoot.js
@@ -0,0 +1,28 @@
+AFRAME.registerComponent("bullets",{
+    init:function(){
+        this.shootBullet();
+    },
+    shootBullet:function(){
+        window.addEventListener("keydown",(e)=>{
+            var bullet = document.createElement("a-entity");
+            bullet.setAttribute("geomentry",{
+                primitive: "sphere",
+                radius:0.1,
+            });
+            bullet.setAttribute("material","color","black");
+            var cam = document.querySelector("#camera");
+            pos = cam.getAttribute("position");
+
+            bullet.setAttribute("position",{
+                x:pos.x,
+                y:pos.y,
+                z:pos.z,
+            });
+
+            var camera = document.querySelector("#camera").object3D;
+
+         })
+
+
+    }
+})
